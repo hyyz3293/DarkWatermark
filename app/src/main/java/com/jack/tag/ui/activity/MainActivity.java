0e96efdfb85a704cd9ed501b93577fd7e9237e37
hyyz3293 <>
2019-09-03 18:05:21 +0800

    集成OPCV

--- app/src/main/java/com/jack/tag/ui/activity/MainActivity.java
+++ app/src/main/java/com/jack/tag/ui/activity/MainActivity.java
@@ -6,18 +6,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.blankj.utilcode.util.LogUtils;
 import com.blankj.utilcode.util.PermissionUtils;
 import com.bumptech.glide.Glide;
 import com.jack.tag.R;
+import com.jack.tag.utils.DFTUtil;
 import com.luck.picture.lib.PictureSelector;
 import com.luck.picture.lib.config.PictureConfig;
 import com.luck.picture.lib.config.PictureMimeType;
 import com.luck.picture.lib.entity.LocalMedia;
+
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,6 +44,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         initView();
         initData();
+
+        //初始化
+        if (OpenCVLoader.initDebug()) {
+            LogUtils.e("success");
+        }else  {
+            LogUtils.e("fail");
+        }
     }
 
     private void initView() {
@@ -40,10 +58,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.tv_change).setOnClickListener(this);
         findViewById(R.id.tv_add).setOnClickListener(this);
         findViewById(R.id.tv_extract).setOnClickListener(this);
+
+        findViewById(R.id.gary_test).setOnClickListener(this);
     }
 
     private void initData() {
         selectList = new ArrayList<>();
+
+        Glide.with(this).load(R.mipmap.b).into(img);
     }
 
     @Override
@@ -58,8 +80,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.tv_extract:
 
                 break;
+            case R.id.gary_test:
+                convert2Grey();
+                break;
         }
     }
+
+
+    /** 灰度测试 代码 */
+    private void convert2Grey() {
+        Mat src = new Mat();//Mat是OpenCV的一种图像格式
+        Mat temp = new Mat();
+        Mat dst = new Mat();
+        Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(), R.mipmap.b);
+        Utils.bitmapToMat(bitmap, src);
+        Imgproc.cvtColor(src, temp, Imgproc.COLOR_RGB2BGR);
+        Imgproc.cvtColor(temp, dst, Imgproc.COLOR_BGR2GRAY);
+        Utils.matToBitmap(dst, bitmap);
+        img.setImageBitmap(bitmap);
+        src.release();
+        temp.release();
+        dst.release();
+    }
+
 
     @SuppressLint("WrongConstant")
     private void openImageSel() {
